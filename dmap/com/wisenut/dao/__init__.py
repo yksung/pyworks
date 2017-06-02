9ffa31fbd98320f86169d2be310a46fea2962761
Holly <Holly@Holly-THINK>
2017-06-02 22:17:18 +0900

    검색 및 엑셀 생성 완성.

--- dmap/com/wisenut/dao/__init__.py
+++ dmap/com/wisenut/dao/__init__.py
@@ -64,31 +64,35 @@ def get_es_data(params, size, scroll_id=None):
         } 
     
     must = []
-    must_not = []
     # 대상 채널
     if "channel" in params and params["channel"]:
         channel = params["channel"]
-        depth1_seqs, depth2_seqs = channel.split(";") if len(channel.split(";")) > 1 else channel.split(";"), None
         
-        query = " OR ".join(depth1_seqs.split(","))
-        query += " AND " + " OR ".join(depth2_seqs.split(",")) if depth2_seqs else ""
+        query = ''
+        for c in channel.split(";"):
+            depth1_seq = c.split("^")[0]
+            depth2_seq = c.split("^")[1]
         
+            query += "("
+            query += "depth1_seq:"+depth1_seq
+            query += " AND depth2_seq:("+" OR ".join(depth2_seq.split(","))+")"
+            query += ")"
+            
+            query += " OR "
+            
         must.append({
             "query_string": {
-                "query": query
+                "query": re.compile(" OR $").sub("", query)
             }
         })
      
     # 대상 기간   
-    if "period" in params and params["period"]:
-        period = params["period"]
-        start_date, end_date = period.split("~") if len(period.split("~")) > 1 else period.split("~"), "2100-12-31 23:59:59"
-        
+    if "start_date" in params and "end_date" in params:
         must.append({
             "range": {
                 "doc_datetime": {
-                    "gte" : start_date,
-                    "lte" : end_date
+                    "gte" : params["start_date"] if params["start_date"] else "1900-01-01 00:00:00",
+                    "lte" : params["end_date"] if params["end_date"] else "2100-12-31 23:59:59"
                 } 
             }
         })
@@ -96,77 +100,98 @@ def get_es_data(params, size, scroll_id=None):
     # 데이터셋의 포함 키워드
     if "datasets" in params and params["datasets"]: # 신라면,삼양라면,안성탕면
         should = [] # 신라면 OR 삼양라면 OR 안성탕면
-        query = get_include_keywords(params["datasets"]) # dataset 시퀀스로 dataset_keyword 조회
+        result_list = get_include_keywords(params["datasets"]) # dataset 시퀀스로 dataset_keyword 조회
         
-        should.append({
-            "query_string": {
-                "fields": ["doc_title", "doc_content"],
-                "query" : query, # (신라면 농심 nongshim) OR (辛라면 농심) OR (푸라면 놈심)
-                "default_operator": "AND"
+        for result in result_list:
+            this_must = {}
+            this_must_not = []
+            this_must = {
+                "query_string": {
+                    "fields": ["doc_title", "doc_content"],
+                    "query" : ",".join([result["keyword"],result['sub_keywords']]).replace(",", " AND ") # (신라면 농심 nongshim) OR (辛라면 농심) OR (푸라면 놈심)
+                }
+            }
+            # 제목 필터
+            this_must_not.append({
+                "query_string" : {
+                    "query" : "doc_title:("+result['title_filter_keywords'].replace(","," OR ")+")"
+                }
+            })
+            # 본문 필터
+            this_must_not.append({
+                "query_string" : {
+                    "query" : "doc_content:("+result['content_filter_keywords'].replace(","," OR ")+")"
+                }
+            })
+            # URL 필터
+            bool_should = []
+            for url in result['filter_urls'].split(","):
+                bool_should.append({
+                    "match_phrase" : {
+                        "doc_url" : url
+                    }
+                })
+            this_must_not.append({
+                "bool" : {
+                    "should" : bool_should
+                }
+            })
+            
+            should.append({
+                "bool" : {
+                    "must" : this_must,
+                    "must_not" : this_must_not
+                }
+            })
+        must.append({
+            "bool" : {
+                "should" : should
             }
         })
-
-        must.append({"bool" : { "should" : should } })
-            
-    # 캠페인과 데이터셋의 필터 키워드
-    if "datasets" in params and params["datasets"]:
-        query = get_exclude_keywords(params["datasets"]) # dataset 시퀀스로 dataset_keyword 조회
-        
-        must_not.append({
-            "query_string": {
-                "fields": ["doc_title", "doc_content"],
-                "query" : query,
-                "default_operator": "AND"
-            }
-        })       
+      
         
     # elif params["type_cd"] == "CCT002": # 소셜모니터링-문서통계
     # elif params["type_cd"] == "CCT003": # 소셜모니터링-감성분석
     # .....
     # 코드별로 request 필요한 형태로 변경해서 추가
     
-    request["query"]["bool"]["must"] = must
+    if "query" in request:
+        request["query"]["bool"]["must"] = must
+        
+    logger.debug("get_es_data() ==> request : " )
+    for k,v in request.items():
+        logger.debug("\t{} : {}".format(k,v))
     
     es_conn.request("POST", es_uri, json.dumps(request), {"Content-type" : "application/json"})
     
     return json.loads(es_conn.getresponse().read())
        
-def get_exclude_keywords(dataset_seq):
-    conn = pymysql.connect(host=mariadb_ip, port=mariadb_port, user=mariadb_user, password=mariadb_password, db=mariadb_db, charset=mariadb_charset)
-    curs = conn.cursor()
-    sql = "select keyword, sub_keywords from tb_dataset_keyword where use_yn='Y' and dataset_seq=%s"
-     
-    dataset_keywords = curs.execute(sql, dataset_seq)
-    
-    conn.close()
-    
-    return ",".join(list(dataset_keywords)).replace(",", " ")
-
-
 def get_include_keywords(dataset_seq):
     conn = pymysql.connect(host=mariadb_ip, port=mariadb_port, user=mariadb_user, password=mariadb_password, db=mariadb_db, charset=mariadb_charset)
-    curs = conn.cursor()
-    sql = "select keyword, sub_keywords from tb_dataset_keyword where use_yn='Y' and dataset_seq=%s"
+    curs = conn.cursor(pymysql.cursors.DictCursor)
+    sql = """select keyword 
+                      ,sub_keywords
+                      ,title_filter_keywords
+                      ,content_filter_keywords
+                      ,filter_urls
+                from tb_dataset_keyword
+              where use_yn='Y' and dataset_seq=%s"""
      
-    dataset_keywords = curs.execute(sql, dataset_seq)
-    keywords = ""
-    for dataset in dataset_keywords:
-        keywords += "("
-        keywords += ",".join(list(dataset))
-        keywords += ");"
-
-    keywords = re.compile(";$").sub("", keywords).replace(";", " OR ").replace(",", " ")
+    curs.execute(sql, dataset_seq)
+    
+    rows = curs.fetchall()
     
     conn.close()
     
-    return keywords
+    return rows
 
 def get_csv_name(csv_type_cd):
     conn = pymysql.connect(host=mariadb_ip, port=mariadb_port, user=mariadb_user, password=mariadb_password, db=mariadb_db, charset=mariadb_charset)
     curs = conn.cursor()
     sql = "select name from tb_common_code where type_cd=%s"
-     
-    csv_type_name = curs.execute(sql, csv_type_cd)
+    curs.execute(sql, csv_type_cd)
+   
+    csv_type_name = curs.fetchone()[0]
     
     conn.close()
     
@@ -182,7 +207,17 @@ def get_excel_request():
     curs = conn.cursor(pymysql.cursors.DictCursor)
     
     # sql 문 실행
-    sql = """select a.seq, a.user_seq, a.type_cd, a.account_seq, a.reg_dt, a.datasets, a.channel, a.period, a.compare_yn, a.campaign_seq
+    sql = """select a.seq
+                    , a.user_seq
+                    , a.type_cd
+                    , a.account_seq
+                    , DATE_FORMAT(a.reg_dt, "%Y%m%d") as reg_dt
+                    , a.datasets
+                    , a.channel
+                    , DATE_FORMAT(a.start_date, "%Y-%m-%d %T") as start_date
+                    , DATE_FORMAT(a.end_date, "%Y-%m-%d %T") as end_date
+                    , a.compare_yn
+                    , a.campaign_seq
                 from tb_csv_queue a, tb_user b
                where a.status='I'
                   and b.use_yn='Y'
@@ -191,35 +226,30 @@ def get_excel_request():
     
     rows = curs.fetchall()
     for row in rows:
+        logger.debug("get_excel_request() ==> row : ")
+        for k,v in row.items():
+            logger.debug("\t{} : {}".format(k,v))
         req_list.append(row)
                 
     conn.close()
     
     return req_list
 
-def update_queue(data):
+def update_queue(data, flag):
     conn = pymysql.connect(host=mariadb_ip, port=mariadb_port, user=mariadb_user, password=mariadb_password, db=mariadb_db, charset=mariadb_charset)
     curs = conn.cursor()
-    sql = "update tb_csv_queue set status = 'P' where seq=%s" # 전달 받은 request seq에 해당하는 row의 status를 P로 변경.
+    sql = "update tb_csv_queue set status = %s where seq=%s" # 전달 받은 request seq에 해당하는 row의 status를 P로 변경.
     
-    curs.execute(sql, data['seq'])
+    curs.execute(sql, [flag, data['seq']])
     
     conn.commit()
     conn.close()
-    
-def get_dataset_name(dataset_seq):
-    conn = pymysql.connect(host=mariadb_ip, port=mariadb_port, user=mariadb_user, password=mariadb_password, db=mariadb_db, charset=mariadb_charset)
-    curs = conn.cursor()
-    sql = "select name from tb_dataset where seq = %s"
-    
-    row = curs.execute(sql, dataset_seq)
-    
-    conn.close()
-    
-    return row[0]
 
 if __name__ == '__main__':
-    get_es_data({
-                 "channel" : "2,3;11,15",
-                 ""
-                 })
+    es_result = get_es_data({
+                 "channel" : "2^11;3^15",
+                 "reg_dt" : "20170530",
+                 "datasets" : "6", # es에는 dataset seq이 한개씩만 넘어감.
+                 "period" : "2016-01-01 00:00:00~2016-01-31 23:59:59",
+                 }, 10)
+    print(es_result)
--- dmap/com/wisenut/excel_maker.py
+++ dmap/com/wisenut/excel_maker.py
@@ -29,69 +29,72 @@ stream_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-BASE_EXCEL_DIRECTORY='/data/dmap-data/dmap-excel'
+#BASE_EXCEL_DIRECTORY='/data/dmap-data/dmap-excel'
+BASE_EXCEL_DIRECTORY=r'E:\documents'
 crawl_doc_fields = [ 'doc_id','doc_datetime','doc_writer','doc_url','doc_title','doc_content','user_id','view_count','comment_count','like_count','dislike_count','share_count','locations','depth1_nm','depth1_seq','depth2_nm','depth2_seq','depth3_nm','depth3_seq','item_grp_nm','item_grp_seq','item_nm','item_seq' ]
 
 
     
 def search_create_directory(dirname):
     if not os.path.exists(dirname):
-        os.mkdir(dirname)
+        os.makedirs(dirname)
         
     return dirname
     
 
       
-import xlrd, xlsxwriter
+import xlsxwriter
 
 if __name__ == '__main__':
     logger.info("excel maker starts.")
     #01. 엑셀 다운로드 요청 목록을 테이블에서 가져옴.
     for req in dao.get_excel_request():
-        logger.debug(req)
         req_seq = req['seq']
         reg_dt = re.compile("[-:\s]").sub("",req['reg_dt'])
         csv_type_name = dao.get_csv_name(req['type_cd'])
         channel = '전채널' if not req['channel'] or req['channel']=='all' else "채널일부"
-        period = re.compile("[-:\s]").sub("",req['period'])
+        start_date = re.compile("[-:\s]").sub("", req['start_date'])[:8]
+        end_date = re.compile("[-:\s]").sub("", req['end_date'])[:8]
         
         #02. 해당 엑셀에 대해서는 table의 상태값을 바꿈.
-        dao.update_queue(req)
+        dao.update_queue(req, "P")
         
         #03. 각 요청에 해당하는 디렉토리를 찾고(요청시퀀스) 없으면 생성. 파일이름 만듦.
         # 디렉토리 구성 ==> /data/dmap-data/dmap-excel/20170601/12'
-        file_path = search_create_directory( os.path.join( BASE_EXCEL_DIRECTORY, reg_dt, req_seq ) )
-        file_name = "_".join(req_seq, csv_type_name, channel, period)
+        file_path = search_create_directory( os.path.join( BASE_EXCEL_DIRECTORY, reg_dt, str(req_seq) ) )
+        file_name = "_".join([str(req_seq), csv_type_name, channel, start_date, end_date])
         
+        #04. 데이터 검색 및 파일 작성
         if 'datasets' in req:
             for dataset_seq in req['datasets'].split(","): # dataset 하나당 엑셀 하나를 만듦. 따라서 dataset_seq는 한개씩만 넘어감.
-                dataset_name = dao.get_dataset_name(dataset_seq)
-                file_name += "_"+dataset_name
+                this_file_name = file_name + "_"+str(dataset_seq)
         
-                #03. 데이터 가져오기
-                workbook = xlsxwriter.Workbook(file_name+".xlsx", options={'strings_to_urls': False} )
-                worksheet = workbook.add_worksheet()
-                
-                # 엑셀 헤더
-                for colidx, field in enumerate(crawl_doc_fields):
-                    worksheet.write(0, colidx, field)
-                
                 req['datasets'] = dataset_seq # req에서 dataset_seq를 한개 값으로만 다시 세팅해서 넘김.
                 size = 10000 # 페이징 사이즈
+                
+                # 검색 시작
                 result = dao.get_es_data(req, size)
+                
                 if "hits" in result and result["hits"]["total"] > 0:
+                    workbook = xlsxwriter.Workbook(os.path.join(file_path, this_file_name+".xlsx"), options={'strings_to_urls': False} )
+                    worksheet = workbook.add_worksheet()
+                    
+                    # 엑셀 헤더
+                    for colidx, field in enumerate(crawl_doc_fields):
+                        worksheet.write(0, colidx, field)
+                    
                     for row, result in enumerate(result["hits"]["hits"]):
                         for col, field in enumerate(crawl_doc_fields):
-                            worksheet.write(row, col, result["_source"][field])
+                            worksheet.write(row+1, col, result["_source"][field])
                             
                     workbook.close()
                             
                     # 결과건수가 한 페이지 사이즈보다 큰 경우, scroll을 이용해서 paging하며 결과를 가져옴.
                     # 용량이 클 것으로 예상하여 엑셀 파일도 새로 생성.            
-                    if  result["hits"]["total"] > size:
+                    if "hits" in result and result["hits"]["total"] > size:
                         for page in range(1, math.ceil(result["hits"]["total"]/size)): # 0, 1, 2, ....
                             # 파일이름 => 12_소셜모니터링_요인분석_all_20170101~20170131_신라면(1).xlsx
-                            workbook = xlsxwriter.Workbook(file_name+"("+str(page)+")"+".xlsx", options={'strings_to_urls': False} )
+                            workbook = xlsxwriter.Workbook(os.path.join(file_path, this_file_name+"("+str(page)+")"+".xlsx"), options={'strings_to_urls': False} )
                             worksheet = workbook.add_worksheet()
                             
                             # 엑셀 헤더
@@ -101,10 +104,11 @@ if __name__ == '__main__':
                             result = dao.get_es_data(req, size, scroll_id=result["_scroll_id"])
                             for row, result in enumerate(result["hits"]["hits"]):
                                 for col, field in enumerate(crawl_doc_fields):
-                                    worksheet.write(row, col, result["_source"][field])
+                                    worksheet.write(row+1, col, result["_source"][field])
                             
                             workbook.close()
                             
-        
+        #04. 완료 플래그로 바꿈.
+        dao.update_queue(req, "C")
         
         
